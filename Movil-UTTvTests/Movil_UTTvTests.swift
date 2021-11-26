adc40991035725b0254b81a794b49bc8531baa39
RUBENXD713 <19170045@uttcampus.edu.mx>
2021-11-25 19:23:30 -0600

    Initial Commit

---
+++ Movil-UTTvTests/Movil_UTTvTests.swift
@@ -0,0 +1,33 @@
+//
+//  Movil_UTTvTests.swift
+//  Movil-UTTvTests
+//
+//  Created by Ruben Hernandez Diaz on 25/11/21.
+//
+
+import XCTest
+@testable import Movil_UTTv
+
+class Movil_UTTvTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
