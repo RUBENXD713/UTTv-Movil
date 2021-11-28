d8293d873be79b1b18169234130e9e7a4ba83509
RUBENXD713 <19170045@uttcampus.edu.mx>
2021-11-28 02:10:10 -0600

    first commit

---
+++ Movil-UTTv/Video2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Video2ViewController.swift
+//  Movil-UTTv
+//
+//  Created by Ruben Hernandez Diaz on 26/11/21.
+//
+
+import UIKit
+
+class Video2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
